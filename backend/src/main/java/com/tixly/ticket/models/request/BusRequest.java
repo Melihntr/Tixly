358f4de120c4949450ed7e42a818889c51bd87af
Melihntr <melihavci@yahoo.com>
2024-07-25 10:57:14 +0300

    Adjusted changes2

---
+++ backend/src/main/java/com/tixly/ticket/models/request/BusRequest.java
@@ -0,0 +1,15 @@
+package com.tixly.ticket.models.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusRequest {
+    private String plateNo;
+    private int seatNo;
+    private int companyId;
+    private String busType;
+}
